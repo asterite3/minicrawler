fdcbaf188bc62ec2fd2a0f23d9eac3fca6b883b3
asterite <asterite@seclab.cs.msu.ru>
2021-11-01 12:29:30 +0300

    page-events: add events based on element types

--- page-events.js
+++ page-events.js
@@ -5,10 +5,24 @@ const OBJECT_GROUP_NAME = 'event-listeners-group';
 async function getPossibleEvents(page) {
     /*
         click
-        fill
+        input/fill
         submit
         mouseover
+        scroll
     */
+
+    const clickEvents = (await page.$$('a, button')).map(el => ({
+        type: 'click',
+        element: el
+    }));
+    const inputEvents = (await page.$$('input, textarea, select')).map(el => ({
+        type: 'input',
+        element: el
+    }));
+    const submitEvents = (await page.$$('form')).map(el => ({
+        type: 'submit',
+        element: el
+    }));
 
     const { result: html } = await page._client.send('Runtime.evaluate', {
         expression: 'document.documentElement',
@@ -25,7 +39,7 @@ async function getPossibleEvents(page) {
 
     const executionContext = await page.mainFrame().executionContext();
 
-    return await Promise.all(eventListeners.listeners.map(async listener => {
+    const jsEvents = await Promise.all(eventListeners.listeners.map(async listener => {
         const nodeRemoteObject = await page._client.send('DOM.resolveNode', {
             backendNodeId: listener.backendNodeId,
             objectGroup: OBJECT_GROUP_NAME,
@@ -41,6 +55,7 @@ async function getPossibleEvents(page) {
             )
         };
     }));
+    return clickEvents.concat(inputEvents, submitEvents, jsEvents);
 };
 
 exports.getPossibleEvents = getPossibleEvents;
