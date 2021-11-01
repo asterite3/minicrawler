48fb909b278e907c99c67387987ce10e78defc42
asterite <asterite@seclab.cs.msu.ru>
2021-11-01 12:01:07 +0300

    utils: add method withTimeout
    It allows to wrap a promise with timeout. In its current form it
    does not allow to get a resulting value of this promise, it
    returns true if timeout occured and false otherwise.

--- utils.js
+++ utils.js
@@ -4,14 +4,26 @@ function wait(d) {
 
 function waitWithCancel(d) {
     let timeoutID;
+    const timerFired = Symbol();
     const p = new Promise(resolve => {
-        timeoutID = setTimeout(resolve, d);
+        timeoutID = setTimeout(() => resolve(timerFired), d);
     });
     return {
         promise: p,
         cancel: () => clearTimeout(timeoutID),
+        timerFiredSymbol: timerFired
     };
 };
 
+async function withTimeout(p, d) {
+    let {promise: timer, cancel, timerFiredSymbol} = waitWithCancel(d);
+    try {
+        return await Promise.race([p, timer]) === timerFiredSymbol
+    } finally {
+        cancel();
+    }
+}
+
 exports.wait = wait;
 exports.waitWithCancel = waitWithCancel;
+exports.withTimeout = withTimeout;
