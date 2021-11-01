53102199e7467bc0a2a16eb610bf0ff8ecfe6f73
asterite <asterite@seclab.cs.msu.ru>
2021-11-01 13:07:48 +0300

    Entry point: improve logging
    Log start and end events (with URL) and put DEP output into
    'finally' block. This way, event if minicrawler crashes, it will
    output DEPs it found before the crash

--- bin/minicrawler.js
+++ bin/minicrawler.js
@@ -1,6 +1,7 @@
 const { ArgumentParser } = require('argparse');
 
 const { Crawler } = require('..');
+const { log } = require('../logging');
 
 (async () => {
     const parser = new ArgumentParser();
@@ -13,10 +14,12 @@ const { Crawler } = require('..');
     const crawler = new Crawler(args.target_url, !args.no_headless);
 
     try {
+        log(`start crawling ${args.target_url}`);
         await crawler.crawl();
+        log(`completed crawling ${args.target_url}`);
     } finally {
+        log(`crawling process of ${args.target_url} ended, closing and exiting`);
         await crawler.close();
+        console.log(JSON.stringify(crawler.xhrLogger.requests, null, 4));
     }
-
-    console.log(JSON.stringify(crawler.xhrLogger.requests, null, 4));
 })();
