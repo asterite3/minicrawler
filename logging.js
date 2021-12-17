ce39a8de20dbfc1da8ff5b0ce196823568c9967d
asterite <asterite@seclab.cs.msu.ru>
2021-12-17 19:18:32 +0300

    update logging.js

--- logging.js
+++ logging.js
@@ -1,20 +1,13 @@
-const pad = s => String(s).padStart(2, '0');
-const padYear = s => String(s).padStart(4, '0');
-const padMsec = s => String(s).padEnd(3, '0');
-
 function formatDate() {
+    const pad = s => String(s).padStart(2, '0');
     const d = new Date();
     const dateString = `${pad(d.getDate())}.${pad(d.getMonth() + 1)}` +
         `.${d.getFullYear()}`;
-
     const timeString = `${pad(d.getHours())}:${pad(d.getMinutes())}:` +
-        `${pad(d.getSeconds())}.${padMsec(d.getMilliseconds())}`;
-
+        `${pad(d.getSeconds())}.${String(d.getMilliseconds()).padStart(3, '0')}`;
     return `${dateString} ${timeString}`;
 }
 
-function log(...args) {
-    console.error(formatDate(), ...args);
-};
-
-exports.log = log;
+module.exports.log = function log(msg) {
+    console.error(`${formatDate()} ${msg}`);
+}
