b35c6fa3a52682e334bc87c9904f5750a4156e41
asterite <asterite@seclab.cs.msu.ru>
2021-12-20 13:15:48 +0300

    TypeScript definition: specify crawler options
    Until now, crawler options object (passed to ctor) was just
    an 'object', without any specific fields. Now it is an interface
    where possible options are specified. This makes crawler interface
    more clear

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,13 @@
 import type * as puppeteer from 'puppeteer';
 
+interface CrawlerOptions {
+    logXHR?: boolean;
+    waitMode?: string;
+    loadedCooldown?: number;
+    executablePath?: string | null;
+    timeout?: number;
+}
+
 export declare class Crawler {
     page: puppeteer.Page | null;
     pageIsCreated: Promise<void>;
@@ -9,7 +17,7 @@ export declare class Crawler {
         headless?: boolean,
         reqExtraHeaders?: Record<string, string>,
         proxy?: string,
-        options?: object
+        options?: CrawlerOptions
     );
     loadPage(timeout?: number): Promise<puppeteer.HTTPResponse>;
     handleRequest(cb: (req: puppeteer.HTTPRequest) => void): Promise<void>;
